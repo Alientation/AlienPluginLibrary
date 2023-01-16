90f9fe5c035e7d47743fcbee3bc5eb4e1f5f456c
Bill <billma.texas@gmail.com>
2023-01-16 15:34:16 -0600

    comments

--- src/main/java/me/alientation/doomboheadplugin/customcommand/annotations/PermissionAnnotation.java
+++ src/main/java/me/alientation/doomboheadplugin/customcommand/annotations/PermissionAnnotation.java
@@ -7,6 +7,9 @@ import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * Sets the permissions for the command todo incorporate with CustomPermissions
+ */
 @Retention(RUNTIME)
 @Target({ METHOD})
 @Repeatable(PermissionAnnotations.class)
--- src/main/java/me/alientation/doomboheadplugin/customcommand/annotations/PermissionAnnotations.java
+++ src/main/java/me/alientation/doomboheadplugin/customcommand/annotations/PermissionAnnotations.java
@@ -6,6 +6,9 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * Sets the permissions for the command todo incorporate with CustomPermissions
+ */
 @Retention(RUNTIME)
 @Target({METHOD})
 public @interface PermissionAnnotations {

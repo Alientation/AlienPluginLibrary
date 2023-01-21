41aad456ce4ba5a1fa94851805c68d9b247e6a1b
Bill <billma.texas@gmail.com>
2023-01-21 12:30:11 -0600

    deleted parameter optional annotation because parameters passed to methods should never be optionally there

--- src/main/java/me/alientation/doomboheadplugin/customcommand/annotations/parameters/ParameterFlagAnnotation.java
+++ src/main/java/me/alientation/doomboheadplugin/customcommand/annotations/parameters/ParameterFlagAnnotation.java
@@ -6,6 +6,9 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * Flags what kind of parameters is required to forward to a supplied method
+ */
 @Retention(RUNTIME)
 @Target(FIELD)
 public @interface ParameterFlagAnnotation {
